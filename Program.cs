30cfe7fb92d9ca9894984ceb09b9a201cc1de807
karolaban <karolina.x.banach@gmail.com>
2021-09-11 09:42:36 +0200

    Obsługa wieku

--- Program.cs
+++ Program.cs
@@ -8,7 +8,21 @@ namespace Emerytura
         {
             Console.WriteLine("Podaj swoje imię i nazwisko: ");
             string nazwa = Console.ReadLine();
-            Console.WriteLine("Witaj " + nazwa + "!");
+            Console.WriteLine("Witaj {0}!", nazwa);
+
+            Console.WriteLine("Podaj swój wiek: ");
+            string wiekNapis = Console.ReadLine();
+            int wiek = int.Parse(wiekNapis);
+            Console.WriteLine($"Masz {wiek} lat.");
+            if (wiek >= 67)
+            {
+                Console.WriteLine("Jesteś emerytem.");
+            }
+            else
+            {
+                Console.WriteLine($"Do emerytury zostało Ci {67 - wiek} lat.");
+            }
+
 
         }
     }
