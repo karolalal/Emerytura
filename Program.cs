52f62679b832daf4c853b21f6db538ea3ff68170
karolaban <karolina.x.banach@gmail.com>
2021-09-12 11:04:29 +0200

    Obsługa wyjątków

--- Program.cs
+++ Program.cs
@@ -4,26 +4,45 @@ namespace Emerytura
 {
     class Program
     {
+        const int WIEKEMERYTALNY = 65;
         static void Main(string[] args)
         {
             Console.WriteLine("Podaj swoje imię i nazwisko: ");
             string nazwa = Console.ReadLine();
+            nazwa = nazwa.Trim().ToUpper();
             Console.WriteLine("Witaj {0}!", nazwa);
 
             Console.WriteLine("Podaj swój wiek: ");
             string wiekNapis = Console.ReadLine();
-            int wiek = int.Parse(wiekNapis);
+
+            int wiek = 0;
+            try
+            {
+                wiek = int.Parse(wiekNapis);
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Nie podałeś poprawnej liczby.\nKoniec aplikacji.");
+                return;
+            }
+            catch(OverflowException)
+            {
+                Console.WriteLine("Podana wartość przekracza moje możliwości.\nKoniec aplikacji.");
+                return;
+            }
+
             Console.WriteLine($"Masz {wiek} lat.");
-            if (wiek >= 67)
+            if (wiek >= WIEKEMERYTALNY)
             {
                 Console.WriteLine("Jesteś emerytem.");
             }
             else
             {
-                Console.WriteLine($"Do emerytury zostało Ci {67 - wiek} lat.");
+                Console.WriteLine($"Do emerytury zostało Ci {WIEKEMERYTALNY - wiek} lat.");
             }
 
 
         }
+         
     }
 }
